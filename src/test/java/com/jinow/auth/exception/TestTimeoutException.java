bc0f33e23509dbfc6f4dd94c1b35aba149fc4781
최진원 <ca98047@gmail.com>
2021-03-01 17:17:37 +0900

    create SlowTest annotation and check timeout during running test code

---
+++ src/test/java/com/jinow/auth/exception/TestTimeoutException.java
@@ -0,0 +1,8 @@
+package com.jinow.auth.exception;
+
+public class TestTimeoutException extends RuntimeException {
+
+    public TestTimeoutException(String message) {
+        super(message);
+    }
+}
